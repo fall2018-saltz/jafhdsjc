536d432a19c8343f30e04e6f736f37bccb443089
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 02:31:42 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -14,7 +14,7 @@ library(ggplot2)
 
 
 # overallCustSat is on y-axis
-#all the dependent variables are on the x-axis
+#aexplonatory/indipendent variables are on the x-axis
 
 
 #hotelsize vs customer satisfaction
